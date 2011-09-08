1d9dd9efbcc5fc0c5d8be6ca5d92cc427feb7130
Tim Frazier <tim.frazier@gmail.com>
2011-09-07 21:01:51 -0500

    application setup and ready to roll

---
+++ htdocs/index.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ *   This file is responsible for Booting your application.  Little to no modification
+ * should be needed
+ */
+$root = dirname(dirname(__FILE__));
+define("P3\ROOT", $root);
+require($root.'/lib/P3/P3.php');
+
+if(P3::development()) {
+	ini_set("display_errors", "true");
+	error_reporting(E_ALL);
+}
+
+try {
+	P3::boot();
+} catch(P3\Exception\Base $e) {
+	switch ((int)$e->getCode()) {
+		case 404:
+			header("HTTP/1.0 404 Not Found");
+		case 500:
+		default:
+			var_dump($e);
+	}
+}
+
+?>
